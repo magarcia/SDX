58def920e7ed1e8e40054438b4e17d98f496b2ac
Martin Garcia <newluxfero@gmail.com>
2014-04-03 02:38:14 +0200

    Add makefile and update muty

--- S2/muty.erl
+++ S2/muty.erl
@@ -3,11 +3,11 @@
 
 start(Lock, Sleep, Work) ->
     register(l1, spawn(Lock, init,[1, [l2,l3,l4]])),
-    register(l2, spawn(Lock, init,[2, [l1,l3,l4]])),    
+    register(l2, spawn(Lock, init,[2, [l1,l3,l4]])),
     register(l3, spawn(Lock, init,[3, [l1,l2,l4]])),
     register(l4, spawn(Lock, init,[4, [l1,l2,l3]])),
     register(john,   spawn(worker, init, ["John",  l1, Sleep, Work])),
-    register(ringo,  spawn(worker, init, ["Ringo", l2, Sleep, Work])),    
+    register(ringo,  spawn(worker, init, ["Ringo", l2, Sleep, Work])),
     register(paul,   spawn(worker, init, ["Paul",  l3, Sleep, Work])),
     register(george, spawn(worker, init, ["George",l4, Sleep, Work])),
     ok.
---
+++ S2/muty_split.erl
@@ -0,0 +1,63 @@
+-module(muty_split).
+-export([start/3, start/1, stop/0]).
+
+%% @doc Client server
+start(BootServer) ->
+    ServerPid = spawn(fun() -> init_server(BootServer) end),
+    register(myserver, ServerPid).
+
+init_server(BootServer) ->
+    BootServer ! {join, node()},
+    receive
+        {params, Id, Lock, Sleep, Work, Nodes} ->
+            run(Id, Lock, Sleep, Work, Nodes)
+    end.
+
+% Master server
+start(Lock, Sleep, Work) ->
+    io:format("Server muty ~w ~w ~w~n", [Lock, Sleep, Work]),
+    ServerPid = spawn(fun() -> init_server(Lock, Sleep, Work, [node()]) end),
+    register(myserver, ServerPid).
+
+init_server(Lock, Sleep, Work, Nodes) ->
+    receive
+        {join, Node} ->
+            NewNodes = [Node|Nodes],
+            if
+                length(NewNodes) == 4 ->
+                    broadcast(NewNodes, {params, Lock, Sleep, Work, NewNodes}),
+                    run(1, Lock, Sleep, Work, NewNodes);
+                true ->
+                    init_server(Lock, Sleep, Work, NewNodes)
+            end
+    end.
+
+broadcast(PeerList, {params, Lock, Sleep, Work, NewNodes}) ->
+    Fun = fun(Peer, Id) -> {myserver, Peer} ! {params, Id, Lock, Sleep, Work, NewNodes} end,
+    lists:foldl(Fun, 2, PeerList).
+
+
+% Common
+run(Id, Lock, Sleep, Work, Nodes) ->
+    Name = lists:concat(["Beatle ", Id, " ", node()]),
+    register(lock, spawn(Lock, init,[Id, get_locks(Nodes)])),
+    register(worker,   spawn(worker, init, [Name,  lock, Sleep, Work])),
+    ok.
+
+get_locks(Nodes) ->
+    if
+        Nodes == [] ->
+            [];
+        true ->
+            [Head|Tail] = Nodes,
+            if
+                Head == node() ->
+                    get_locks(Tail);
+                true ->
+                    [{lock, Head}|get_locks(Tail)]
+            end
+    end.
+
+stop() ->
+    worker ! stop,
+    lock ! stop.
