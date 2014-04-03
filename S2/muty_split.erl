096f9490ed8e911e768aed2370b9e5664766efd2
Martin Garcia <newluxfero@gmail.com>
2014-04-03 02:41:41 +0200

    update docs in custom muty

--- S2/muty_split.erl
+++ S2/muty_split.erl
@@ -13,7 +13,7 @@ init_server(BootServer) ->
             run(Id, Lock, Sleep, Work, Nodes)
     end.
 
-% Master server
+%% @doc Master server
 start(Lock, Sleep, Work) ->
     io:format("Server muty ~w ~w ~w~n", [Lock, Sleep, Work]),
     ServerPid = spawn(fun() -> init_server(Lock, Sleep, Work, [node()]) end),
@@ -32,18 +32,20 @@ init_server(Lock, Sleep, Work, Nodes) ->
             end
     end.
 
+%% @doc Broadcast messages to clients
 broadcast(PeerList, {params, Lock, Sleep, Work, NewNodes}) ->
     Fun = fun(Peer, Id) -> {myserver, Peer} ! {params, Id, Lock, Sleep, Work, NewNodes} end,
     lists:foldl(Fun, 2, PeerList).
 
 
-% Common
+%% @doc Run "local" lock and worker
 run(Id, Lock, Sleep, Work, Nodes) ->
     Name = lists:concat(["Beatle ", Id, " ", node()]),
     register(lock, spawn(Lock, init,[Id, get_locks(Nodes)])),
     register(worker,   spawn(worker, init, [Name,  lock, Sleep, Work])),
     ok.
 
+%% @doc Get locks for this node
 get_locks(Nodes) ->
     if
         Nodes == [] ->
